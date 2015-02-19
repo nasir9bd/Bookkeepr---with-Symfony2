224a222ba91b2d0e27f9670ff8e5913abfbd15cc
nasir9bd <nasir9bd@gmail.com>
2015-02-20 01:01:51 +0600

    added routing

---
+++ app/cache/dev/twig/f1/d2/b51618ab189b7d31d45fcd2cd4531bab2187c38eb8c67f8d6842196145bd.php
@@ -0,0 +1,32 @@
+<?php
+
+/* BookkeeperHelloWorldBundle:Default:index.html.twig */
+class __TwigTemplate_f1d2b51618ab189b7d31d45fcd2cd4531bab2187c38eb8c67f8d6842196145bd extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello World!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "BookkeeperHelloWorldBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
