87170e668d76a7aa8a43982b2a2c3f3d1dd3d823
nasir9bd <nasir9bd@gmail.com>
2015-02-20 01:19:38 +0600

    added book controller

---
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bookkeeper\ManagerBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BookController extends Controller{
+    public function indexAction(){
+
+    }
+
+    public function showAction($id){
+
+    }
+
+    public function newAction(){
+
+    }
+
+    public function createAction(Request $request){
+
+    }
+
+    public function editAction($id){
+
+    }
+
+    public function updateAction(Request $request, $id){
+
+    }
+
+    public function deleteAction(Request $request, $id){
+
+    }
+}
